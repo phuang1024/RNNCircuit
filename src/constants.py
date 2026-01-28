7a23d92b9bc635ac94f48b3b8de9fda541ec7281
Patrick Huang <huangpatrick16777216@gmail.com>
2026-01-27 19:27:25 -0600

    Convert to ODE RNN.

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,11 @@ import torch
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Inner size is the intermediate size of the 2 layer neural network.
+# Hidden size is the output of that network, which produces the hidden state.
+INNER_SIZE = 32
 HIDDEN_SIZE = 32
+TAU = 100
 
 # This is implemented as the L dimension of the input to the RNN.
 BPTT = 100
--- src/model.py
+++ src/model.py
@@ -5,15 +5,61 @@ from constants import *
 
 
 class CircuitRNN(nn.Module):
+    """
+    The dynamical system as a whole is:
+    input: x(t)
+    output: y(t)
+    hidden state: h(t)
+    network: f(h(t), x(t)) -> h'(t)
+        and g(h(t)) -> y(t)
+
+    This class implements the CT RNN with extra layer.
+    input: dt(t) and x(t)
+    output: y(t)
+    Implements an Euler diff eq solver.
+    """
+
     def __init__(self):
         super().__init__()
 
-        self.rnn = nn.RNN(1, HIDDEN_SIZE)
-        self.output = nn.Linear(HIDDEN_SIZE, 1)
+        self.tanh = nn.Tanh()
 
-    def forward(self, x):
-        output, hidden = self.rnn(x)
-        y = self.output(hidden[0])
+        # Layers for "f" function.
+        self.f_x = nn.Linear(1, INNER_SIZE)
+        self.f_h = nn.Linear(HIDDEN_SIZE, INNER_SIZE)
+        self.f_final = nn.Linear(INNER_SIZE, HIDDEN_SIZE)
+
+        # Layers for the "g" function.
+        self.g = nn.Linear(HIDDEN_SIZE, 1)
+
+    def forward_f(self, x, h):
+        """
+        Implements the "f" function.
+        """
+        return -1 / TAU * x + self.tanh(self.f_final(self.f_x(x) + self.f_h(h)))
+
+    def forward(self, dt, x):
+        """
+        Runs the RNN for an N step sequence, using Euler integration.
+        dt, x: shape (B, N)
+            dt is the time step size for each iteration.
+        return: y shape (B, N)
+        """
+        B, N = dt.shape
+
+        # Initialize hidden state to 0.
+        h = torch.zeros([B, HIDDEN_SIZE])
+        # Output tensor.
+        y = torch.zeros([B, N])
+        for i in range(dt.shape[1]):
+            # Take current index of input.
+            xi = x[:, i].unsqueeze(-1)
+            # Compute f and g.
+            h_prime = self.forward_f(xi, h)
+            y[:, i] = self.g(h).squeeze(-1)
+
+            # Apply h'
+            h += h_prime * dt[:, i]
 
         return y
 
@@ -21,6 +67,8 @@ class CircuitRNN(nn.Module):
 if __name__ == "__main__":
     model = CircuitRNN()
 
-    x = torch.randn(100, 32, 1)
-    y = model(x)
+    dt = torch.full((32, 100), 0.01)
+    x = torch.randn(32, 100)
+
+    y = model(dt, x)
     print(y.shape)
